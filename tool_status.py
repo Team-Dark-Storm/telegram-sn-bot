e9cf4e2c9883028b879d6a4cd60d8f6a2e79a645
Team-Dark-Storm <88400419+Team-Dark-Storm@users.noreply.github.com>
2026-01-03 09:35:03 +0200

    Update tool_status.py

--- tool_status.py
+++ tool_status.py
@@ -1,30 +1,14 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# =========================
-# إعدادات التحكم
-# =========================
-TOOL_ENABLED = False        # False = يقفل البرنامج
-# =========================
-# فحص حالة التول
-# =========================
+TOOL_ENABLED = True
+
 @app.route("/tool_status")
 def tool_status():
     return "ENABLED" if TOOL_ENABLED else "DISABLED"
 
-
-
-
-# =========================
-# تشغيل السيرفر
-# =========================
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-
-
-
-
-
-
