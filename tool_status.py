87b09023392f2c543d2b889690e17af8ce4e60e4
Team-Dark-Storm <88400419+Team-Dark-Storm@users.noreply.github.com>
2026-01-03 09:33:00 +0200

    Update tool_status.py

--- tool_status.py
+++ tool_status.py
@@ -20,7 +20,8 @@ def tool_status():
 # تشغيل السيرفر
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+   port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 
 
