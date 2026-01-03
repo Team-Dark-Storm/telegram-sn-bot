33c9494443d55385fb5e1515fb0630484d5646f7
Team-Dark-Storm <88400419+Team-Dark-Storm@users.noreply.github.com>
2026-01-03 09:35:58 +0200

    Update tool_status.py

--- tool_status.py
+++ tool_status.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-TOOL_ENABLED = True
+TOOL_ENABLED = False
 
 @app.route("/tool_status")
 def tool_status():
@@ -12,3 +12,4 @@ def tool_status():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
